aaa27d25ce9f11547abdf17a8701fa6a9c4d45f6
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-22 20:55:41 -0700

    moved weather function into a module and set up error handling.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
 const { use } = require('express/lib/application');
-// const { response } = require('express');
+const getWeather = require('./weather.js');
 
 //  globals
 const app = express();
@@ -15,13 +15,6 @@ const PORT = process.env.PORT || 3002; // added fallback port
 //  use
 app.use(cors());
 
-//  classes
-class Forecast {
-  constructor(date, description) {
-    this.date = date;
-    this.description = description;
-  }
-}
 
 class Movie {
   constructor(movieObj) {
@@ -37,38 +30,21 @@ app.get('/', (req, res) => {
   res.status(420).send('Ehlow orld.');
 });
 
-async function getWeather(cityName) {
-  let params = {
-    url: 'http://api.weatherbit.io/v2.0/forecast/daily',
-    city: cityName,
-    country: 'US',
-    days: 3,
-    units: 'S',
-    api_key: process.env.REACT_APP_WEATHERBIT_API_KEY
-  };
-
-  let wxUrl = `${params.url}?city=${params.city}&country=${params.country}&days=${params.days}&units=${params.units}&key=${params.api_key}`;
-  let wxResponse = await axios.get(wxUrl);
-  let wxResData = wxResponse.data;
-  let wxResDataData = wxResData.data;
-  let forecasts = wxResDataData.map((dayForecast) => new Forecast(dayForecast.datetime, dayForecast.weather.description));
-  return forecasts;
-}
-
 //  weather
-app.get('/weather', async (req, res) => {
-  let clientRequest = req.query.city;
-  let cityQuery = clientRequest ? clientRequest : '';
-  //
-  // see https://en.wikipedia.org/wiki/List_of_short_place_names
-  if (cityQuery.length < 1) {
-    res.status(400).send('Input too short. Add letters, get betters.');
-  }
+app.get('/weather', getWeather);
+// app.get('/weather', async (req, res) => {
+//   let clientRequest = req.query.city;
+//   let cityQuery = clientRequest ? clientRequest : '';
+//   //
+//   // see https://en.wikipedia.org/wiki/List_of_short_place_names
+//   if (cityQuery.length < 1) {
+//     res.status(400).send('Input too short. Add letters, get betters.');
+//   }
 
-  let result = await getWeather(cityQuery);
-  res.status(200).send(result);
-  //res.status(501).send('Not Implemented. Come back later.');
-});
+//   let result = await getWeather(cityQuery);
+//   res.status(200).send(result);
+//   //res.status(501).send('Not Implemented. Come back later.');
+// });
 
 async function getMovies(cityName) {
   console.log('getMovies(cityName): ', cityName);
